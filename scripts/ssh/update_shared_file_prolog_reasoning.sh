7fcf9c79a562745c7d906d404ad429d8805e7b37
Alberto Olivares Alarcos <alberto.olivares.alarcos@gmail.com>
2025-08-06 14:52:04 +0200

    minor change to only copy the content of files not the HTML content

--- scripts/ssh/update_shared_file_prolog_reasoning.sh
+++ scripts/ssh/update_shared_file_prolog_reasoning.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 TARGET_DIR="$SCRIPT_DIR/../../src/prolog_reasoning/know_plan_reasoning"
 
 # Download the target files
-curl -o "$TARGET_DIR/plan_comparison.pl" https://github.com/albertoOA/know_plan/blob/main/src/prolog_reasoning/comparative/plan_comparison.pl
-curl -o "$TARGET_DIR/__init__.pl" https://github.com/albertoOA/know_plan/blob/main/src/prolog_reasoning/comparative/__init__.pl
+curl -o "$TARGET_DIR/plan_comparison.pl" https://raw.githubusercontent.com/albertoOA/know_plan/main/src/prolog_reasoning/comparative/plan_comparison.pl
+curl -o "$TARGET_DIR/__init__.pl" https://raw.githubusercontent.com/albertoOA/know_plan/main/src/prolog_reasoning/comparative/__init__.pl
 
 echo "File updated!"
