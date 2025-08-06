c26ddf989a6204bc95547a0aa4052f1fbf400e52
Alberto Olivares Alarcos <alberto.olivares.alarcos@gmail.com>
2025-08-06 14:45:11 +0200

    updated to solve path issues

--- scripts/ssh/update_shared_file_prolog_reasoning.sh
+++ scripts/ssh/update_shared_file_prolog_reasoning.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Replace with the actual URL of the raw file
-curl -o ../../src/prolog_reasoning/know_plan_reasoning/plan_comparison.pl https://github.com/albertoOA/know_plan/blob/main/src/prolog_reasoning/comparative/plan_comparison.pl
-curl -o ../../src/prolog_reasoning/know_plan_reasoning/__init__.pl https://github.com/albertoOA/know_plan/blob/main/src/prolog_reasoning/comparative/__init__.pl
+# Get the directory of the script, regardless of where it's called from
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Move to the target directory
+TARGET_DIR="$SCRIPT_DIR/../../src/prolog_reasoning/know_plan_reasoning"
+
+# Download the target files
+curl -o "$TARGET_DIR/plan_comparison.pl" https://github.com/albertoOA/know_plan/blob/main/src/prolog_reasoning/comparative/plan_comparison.pl
+curl -o "$TARGET_DIR/__init__.pl" https://github.com/albertoOA/know_plan/blob/main/src/prolog_reasoning/comparative/__init__.pl
 
 echo "File updated!"
